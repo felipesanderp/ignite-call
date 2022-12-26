a3a629bf69a1849d7ce9e55a858852ef5a932ee1
Felipe Sander <felipe.panisson@hotmail.com>
2022-12-26 17:33:37 -0300

    home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <Container>
       <Hero>
         <Heading size="4xl">Agendamento descomplicado</Heading>
-        <Text size="lg">
+        <Text size="xl">
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </Text>
