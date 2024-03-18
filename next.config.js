600be0dccc28e9b97e9860d143dbe5021efa0e1d
felipesanderp <felipe.panisson@hotmail.com>
2024-03-18 16:42:07 -0300

    update cors config

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           {
             key: 'Access-Control-Allow-Origin',
-            value: 'https://ignite-call-taupe.vercel.app',
+            value: '*',
           },
           {
             key: 'Access-Control-Allow-Methods',
